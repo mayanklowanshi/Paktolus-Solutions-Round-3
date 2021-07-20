741d36954d456be8dd740adf84f41027035c298e
mayanklowanshi <74091130+mayanklowanshi@users.noreply.github.com>
2021-07-21 01:41:10 +0530

    boundary case update

--- code.cpp
+++ code.cpp
@@ -1,10 +1,12 @@
 // First line of input contain the number of segments in the array
 // second line of input contain space seperated segments
 
+// Note: Output is 1-based index
+
 #include <iostream>
  
 using namespace std;
-
+ 
 int main()
 {	
 	int n;
@@ -14,6 +16,7 @@ int main()
     for(int j=0;j<n;j++){
         cin>>arr[j];
     }
+
 
     // skipping the expanding spiral
     int l=0,r=0,t=0,b=0,i=0;
@@ -37,7 +40,7 @@ int main()
         if(i>3 && arr[i-2]-arr[i-4]>arr[i]){
             l=r=arr[i-1];
         }
-        else if(i<=3){
+        else if(i<=3 && arr[i]< arr[i-2]){
             l=r=arr[i-1];
         }
         else l=r=arr[i-1]-arr[i-3];
